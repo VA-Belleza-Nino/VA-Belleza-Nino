f51ef92ded31b4aa7f5467cefbb3c4e28c193bf9
VA-Belleza-Nino <nino.belleza@via-appia.ph>
2024-03-20 18:09:00 +0800

    Practice 1

---
+++ Practice1.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  const pi = 7;
+  int p = 5;
+  int t = 10;
+  int r = 15;
+  int y = p * t * r;
+  num z = y / 100;
+
+  print("1.\tNino Bellezaa");
+
+  print('2.\tHello I am "John Doe"');
+  print("\tHello I'm" + ' "John Doe"');
+
+  print("3.\tValue of PI is $pi");
+
+  print("4.\t$z");
+
+}
